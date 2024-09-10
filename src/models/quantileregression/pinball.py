553da531c3051912ffe39116347fc8606cf2ab85
Erik Linde <erik.linde@einride.tech>
2024-09-10 15:27:18 +0200

    fix: adapt crossval scoring

---
+++ src/models/quantileregression/pinball.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+from sklearn.metrics import make_scorer
+
+
+class PinballLoss(nn.Module):
+    def __init__(self, quantile: float):
+        super().__init__()
+        self.quantile = quantile
+
+    def forward(self, yPred, yTrue):
+        errors = yTrue - yPred
+        loss = torch.max(self.quantile * errors, (self.quantile - 1) * errors)
+        return torch.mean(loss)
+    
+def negPinballLossValue(yTrue, yPred, quantile):
+    yTrueTensor = torch.tensor(yTrue, dtype=torch.float32)
+    yPredTensor = torch.tensor(yPred, dtype=torch.float32)
+    
+    lossFunc = PinballLoss(quantile=quantile) 
+    lossValue = lossFunc(yTrueTensor, yPredTensor).item()
+    
+    return -lossValue
+
+def pinballLossScorer(quantile):
+    scorerFunc = lambda yTrue, yPred: negPinballLossValue(yTrue, yPred, quantile)
+    scorer = make_scorer(scorerFunc, greater_is_better=False)
+    return scorer

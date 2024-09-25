69b6a4daab65814dde05a1ee77bec9ce6bfe8876
Erik Linde <erik.linde@einride.tech>
2024-09-25 14:26:51 +0200

    feat(features): add forward features selection
    add forward feature selection (previously only had backward)
    in this, made feature selection into abstract class
    forward and backward selection inherit from this class
    tests modified to include cases for floats, cat, and mixed data

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -4,36 +4,64 @@ Script for investigating feature selection
 
 from sklearn.ensemble import RandomForestRegressor
 
+
 from data.data_loader import loadDataParquet
+from data.data_processing import transformTimestamp
 from models.model import Model
-from features.feature_selection import getBestFeatures
+from features.feature_selection import BackwardFeatureSelector, ForwardFeatureSelector
 
 
 def main():
     dirParquet = "data/intermediate/"
     df = loadDataParquet(dirParquet)
+    df = transformTimestamp(df, "Timestamp", timeOfDayCol="Time_of_day")
 
-    dependentCol = "UL_bitrate"
-
+    dependentCol = "DL_bitrate"
     selectedFloatCols = [
         "Longitude",
         "Latitude",
         "Speed",
         "RSRP",
         "RSRQ",
+        "RSSI",
         "SNR",
+        "CQI",
         "NRxRSRP",
         "NRxRSRQ",
-        "PINGAVG",
+        "Time_of_day",
     ]
-    selectedCatCols = ["CellID"]
-
-    model = Model(RandomForestRegressor(), "Random Forest")
-
-    bestFloatCols = getBestFeatures(
-        df, selectedFloatCols, selectedCatCols, dependentCol, model
+    selectedCatCols = [
+        "CellID",
+        "NetworkMode",
+        "Operatorname",
+        "CELLHEX",
+        "NODEHEX",
+        "LACHEX",
+        "RAWCELLID",
+        "State",
+    ]
+    rf = RandomForestRegressor(
+        n_estimators=300,
+        max_depth=20,
+        min_samples_split=5,
+        min_samples_leaf=2,
+        max_features="sqrt",
     )
-    print(bestFloatCols)
+    model = Model(rf, "Random Forest")
+
+    backwardSelector = BackwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = backwardSelector.getBestFeatures(
+        df, selectedFloatCols, selectedCatCols, dependentCol
+    )
+    print(f"Best float columns: {bestFloatCols}")
+    print(f"Best categorical columns: {bestCatCols}")
+
+    forwardSelector = ForwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = forwardSelector.getBestFeatures(
+        df, selectedFloatCols, selectedCatCols, dependentCol
+    )
+    print(f"Best float columns: {bestFloatCols}")
+    print(f"Best categorical columns: {bestCatCols}")
 
 
 main()
--- tests/test_feature_selection.py
+++ tests/test_feature_selection.py
@@ -8,39 +8,165 @@ from sklearn.linear_model import LinearRegression
 import pytest
 
 from models.model import Model
-from features.feature_selection import getBestFeatures, dropWorseFeature
+
+from features.feature_selection import BackwardFeatureSelector, ForwardFeatureSelector
 
 
 @pytest.fixture
-def sampleDf():
+def sampleFloatDf():
     floatCols = ["feature1", "feature2"]
     catCols = []
     dependentCol = "target"
 
     df = pd.DataFrame(
         {
-            floatCols[0]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            floatCols[1]: np.random.rand(10),
-            dependentCol: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            floatCols[0]: np.arange(1, 101),
+            floatCols[1]: np.random.rand(100),
+            dependentCol: np.arange(1, 101),
         }
     )
 
     return df, floatCols, catCols, dependentCol
 
 
-def test_getBestFeatures(sampleDf):
-    df, floatCols, catCols, dependentCol = sampleDf
+@pytest.fixture
+def sampleCatDf():
+    floatCols = []
+    catCols = ["feature1", "feature2"]
+    dependentCol = "target"
+
+    df = pd.DataFrame(
+        {
+            catCols[0]: ["a"] * 50 + ["b"] * 50,
+            catCols[1]: ["a", "b"] * 50,
+            dependentCol: [1] * 50 + [10] * 50,
+        }
+    )
+
+    return df, floatCols, catCols, dependentCol
+
+
+@pytest.fixture
+def sampleMixedDfV1():
+    floatCols = ["feature1"]
+    catCols = ["feature2"]
+    dependentCol = "target"
+
+    df = pd.DataFrame(
+        {
+            floatCols[0]: np.random.rand(100),
+            catCols[0]: ["a"] * 50 + ["b"] * 50,
+            dependentCol: [1] * 50 + [10] * 50,
+        }
+    )
+
+    return df, floatCols, catCols, dependentCol
+
+
+@pytest.fixture
+def sampleMixedDfV2():
+    floatCols = ["feature1"]
+    catCols = ["feature2"]
+    dependentCol = "target"
+
+    df = pd.DataFrame(
+        {
+            floatCols[0]: np.arange(1, 101),
+            catCols[0]: ["a", "b"] * 50,
+            dependentCol: np.arange(1, 101),
+        }
+    )
+
+    return df, floatCols, catCols, dependentCol
+
+
+def test_backwardFeatureSelectionFloat(sampleFloatDf):
+    df, floatCols, catCols, dependentCol = sampleFloatDf
 
     model = Model(LinearRegression(), "Linear Regression")
-    bestFloatCols = getBestFeatures(df, floatCols, catCols, dependentCol, model)
+    selector = BackwardFeatureSelector(model)
+    bestFloatCols, _ = selector.getBestFeatures(df, floatCols, catCols, dependentCol)
 
     assert bestFloatCols == [floatCols[0]]
 
 
-def test_dropWorseFeature(sampleDf):
-    df, floatCols, catCols, dependentCol = sampleDf
+def test_forwardFeatureSelectionFloat(sampleFloatDf):
+    df, floatCols, catCols, dependentCol = sampleFloatDf
 
     model = Model(LinearRegression(), "Linear Regression")
-    dropFloatCol, _ = dropWorseFeature(df, floatCols, catCols, dependentCol, model)
+    selector = ForwardFeatureSelector(model)
+    bestFloatCols, _ = selector.getBestFeatures(df, floatCols, catCols, dependentCol)
 
-    assert dropFloatCol == floatCols[1]
+    assert bestFloatCols == [floatCols[0]]
+
+
+def test_backwardFeatureSelectionCat(sampleCatDf):
+    df, floatCols, catCols, dependentCol = sampleCatDf
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = BackwardFeatureSelector(model)
+    _, bestCatCols = selector.getBestFeatures(df, floatCols, catCols, dependentCol)
+
+    assert bestCatCols == [catCols[0]]
+
+
+def test_forwardFeatureSelectionCat(sampleCatDf):
+    df, floatCols, catCols, dependentCol = sampleCatDf
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = ForwardFeatureSelector(model)
+    _, bestCatCols = selector.getBestFeatures(df, floatCols, catCols, dependentCol)
+
+    assert bestCatCols == [catCols[0]]
+
+
+def test_backwardFeatureSelectionMixedV1(sampleMixedDfV1):
+    df, floatCols, catCols, dependentCol = sampleMixedDfV1
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = BackwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = selector.getBestFeatures(
+        df, floatCols, catCols, dependentCol
+    )
+
+    assert bestFloatCols == []
+    assert bestCatCols == [catCols[0]]
+
+
+def test_forwardFeatureSelectionMixedV1(sampleMixedDfV1):
+    df, floatCols, catCols, dependentCol = sampleMixedDfV1
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = ForwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = selector.getBestFeatures(
+        df, floatCols, catCols, dependentCol
+    )
+
+    assert bestFloatCols == []
+    assert bestCatCols == [catCols[0]]
+
+
+def test_backwardFeatureSelectionMixedV2(sampleMixedDfV2):
+    df, floatCols, catCols, dependentCol = sampleMixedDfV2
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = BackwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = selector.getBestFeatures(
+        df, floatCols, catCols, dependentCol
+    )
+
+    assert bestFloatCols == [floatCols[0]]
+    assert bestCatCols == []
+
+
+def test_forwardFeatureSelectionMixedV2(sampleMixedDfV2):
+    df, floatCols, catCols, dependentCol = sampleMixedDfV2
+
+    model = Model(LinearRegression(), "Linear Regression")
+    selector = ForwardFeatureSelector(model)
+    bestFloatCols, bestCatCols = selector.getBestFeatures(
+        df, floatCols, catCols, dependentCol
+    )
+
+    assert bestFloatCols == [floatCols[0]]
+    assert bestCatCols == []

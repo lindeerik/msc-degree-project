a925c3ad23dfbb131f15e87191d472e4e437f82f
Erik Linde <erik.linde@einride.tech>
2024-10-08 16:07:27 +0200

    feat(data): add sthlm-sodertalje data
    add raw and cleaned data including script for this
    scrub rows where iperf test stopped and passing through tunnels

---
+++ scripts/sthlm_sodertalje_data_processing.py
@@ -0,0 +1,30 @@
+"""
+Script for cleaning raw data recorded between Stockholm and Södertälje
+"""
+
+import pandas as pd
+
+def main():
+    dirCsv = "data/raw/sthlm-sodertalje/"
+    fileName = "2024.10.04_11.19.11.csv"
+    dfRaw = pd.read_csv(dirCsv + fileName, na_values=["", "-"])
+    df = dfRaw.dropna(axis=1, how="all")
+
+    # drop rows due to stops in iperf test at 11:40, 11:55, 12:22, 12:35
+    # also iperf testing ends at 12:44
+    indicesToDropTestStops = (
+        list(range(2160, 2176))
+        + list(range(3939, 3949))
+        + list(range(6508, 6553))
+        + list(range(8063, 8084))
+        + list(range(8689, len(df)))
+    )
+
+    # drop rows due to tunnels at 11:21, 11:25
+    indicesToDropTunnels = list(range(269, 363)) + list(range(530, 711))
+    dfClean = df.drop(df.index[indicesToDropTestStops + indicesToDropTunnels])
+
+    dirClean = "data/intermediate/sthlm-sodertalje/"
+    dfClean.to_csv(dirClean + fileName)
+
+main()

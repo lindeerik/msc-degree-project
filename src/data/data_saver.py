f3ac9cba5f72950e554c85a8efefdd60bb162adf
Erik Linde <erik.linde@einride.tech>
2024-10-25 09:23:06 +0200

    fix(data-saver): improve robustness of save csv with timestamp
    creates a new directory if directory does not exist
    also uses os for appending path better

--- src/data/data_saver.py
+++ src/data/data_saver.py
@@ -2,11 +2,14 @@
 Data saving for tabular data
 """
 
+import os
 from datetime import datetime
 
 
-
 def saveCsvWithDateTime(data, path):
-    timestamp = datetime.now().strftime('%Y%m%dT%H%M%SZ')
-    filename = f"{path}{timestamp}.csv"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    timestamp = datetime.now().strftime("%Y%m%dT%H%M%SZ")
+    filename = os.path.join(path, timestamp + ".csv")
     data.to_csv(filename, index=False)

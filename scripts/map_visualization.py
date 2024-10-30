b535999383fe61ebd6fad9972ceac9b3a70179ee
Erik Linde <erik.linde@einride.tech>
2024-10-30 10:24:04 +0100

    feat: add more sthlm-sodertalje drive tests
    add drive tests from evening 2024-10-28 and morning 2024-10-29
    adapt scripts for the multiple drive tests

--- scripts/map_visualization.py
+++ scripts/map_visualization.py
@@ -1,26 +1,63 @@
 """
-Map visualization for uplink throughput
+Map visualization for uplink throughput between Stockholm-Södertälje
 """
 
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
 
-from data.data_loader import loadDataCsv
-from data.data_processing import transformTimestamp
 from visualization.maps import plotFloatMap, plotCategoricalMap
+from visualization.save_figures import saveFiguresWithDateTime
 
 
-def stockholmSodertaljeVisualization():
+def main():
+    stockholmSodertaljeVisualization20241004()
+    stockholmSodertaljeVisualization20241028()
+    stockholmSodertaljeVisualization20241029()
+    saveFiguresWithDateTime("figures/")
+
+
+def stockholmSodertaljeVisualization20241004():
+    fileName = "2024.10.04_11.19.11.csv"
     dataDir = "data/intermediate/sthlm-sodertalje/"
-    df = loadDataCsv(dataDir, "")
+    date = "2024-10-04"
     dependentCol = "UL_bitrate"
     threshholdCol = "UL_threshhold"
+    threshholds = [15, 30]
+    stockholmSodertaljeVisualization(
+        fileName, dataDir, date, dependentCol, threshholdCol, threshholds
+    )
+
+
+def stockholmSodertaljeVisualization20241028():
+    fileName = "2024.10.28_17.20.20.csv"
+    dataDir = "data/intermediate/sthlm-sodertalje/"
+    date = "2024-10-28"
+    dependentCol = "UL_bitrate"
+    threshholdCol = "UL_threshhold"
+    threshholds = [15, 30]
+    stockholmSodertaljeVisualization(
+        fileName, dataDir, date, dependentCol, threshholdCol, threshholds
+    )
+
+
+def stockholmSodertaljeVisualization20241029():
+    fileName = "2024.10.29_07.18.51.csv"
+    dataDir = "data/intermediate/sthlm-sodertalje/"
+    date = "2024-10-29"
+    dependentCol = "UL_bitrate"
+    threshholdCol = "UL_threshhold"
+    threshholds = [15, 30]
+    stockholmSodertaljeVisualization(
+        fileName, dataDir, date, dependentCol, threshholdCol, threshholds
+    )
+
+
+def stockholmSodertaljeVisualization(
+    fileName, dataDir, date, dependentCol, threshholdCol, threshholds
+):
+    df = pd.read_csv(dataDir + fileName)
     # Mbps from Kbps
     df[dependentCol] = df[dependentCol] / 1024
-    df = transformTimestamp(
-        df, "Timestamp", timeOfDayCol="Time_of_day", timeOfYearCol="Time_of_year"
-    )
 
     gdf = gpd.GeoDataFrame(
         df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326"
@@ -30,99 +67,28 @@ def stockholmSodertaljeVisualization():
     plotFloatMap(
         gdf,
         dependentCol,
-        title="Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test 2024-10-04",
+        title=f"Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test {date}",
         useLogScale=True,
         alpha=0.1,
     )
 
-    gdf[threshholdCol] = gdf[dependentCol] >= 15
-    customLabels = {False: "<15 mbps", True: ">=15 mbps"}
-    plotCategoricalMap(
-        gdf,
-        threshholdCol,
-        title="Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test 2024-10-04",
-        alpha=0.1,
-        customLabels=customLabels,
-    )
     plotCategoricalMap(
         gdf,
         "NetworkMode",
-        title="Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test 2024-10-04",
+        title=f"Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test {date}",
         alpha=0.1,
     )
 
-
-def ooklaE4Visualization():
-    path = "data/raw/ookla/SDK_data_e4_polygon.csv"
-    df = pd.read_csv(path)
-    selectedCols = ["longitude", "latitude", "ul_throughput"]
-    df = df[selectedCols].dropna()
-    dependentCol = "ul_throughput"
-    threshholdCol = "ul_threshhold"
-
-    gdf = gpd.GeoDataFrame(
-        df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326"
-    )
-    gdf = gdf.to_crs(epsg=3857)
-
-    plotFloatMap(
-        gdf,
-        dependentCol,
-        title="Uplink Throughput (Mbps) E4 Ookla Data",
-        useLogScale=True,
-        alpha=0.1,
-    )
-
-    gdf[threshholdCol] = gdf[dependentCol] >= 15
-    customLabels = {False: "<15 mbps", True: ">=15 mbps"}
-    plotCategoricalMap(
-        gdf,
-        threshholdCol,
-        title="Uplink Throughput (Mbps) E4 Ookla Data",
-        alpha=0.1,
-        customLabels=customLabels,
-    )
-
-
-def ooklaI45Visualization():
-    path = "data/raw/ookla/SDK_data_i45_polygon.csv"
-    df = pd.read_csv(path)
-    selectedCols = ["longitude", "latitude", "ul_throughput"]
-    df = df[selectedCols].dropna()
-    dependentCol = "ul_throughput"
-    threshholdCol = "ul_threshhold"
-
-    gdf = gpd.GeoDataFrame(
-        df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326"
-    )
-    gdf = gdf.to_crs(epsg=3857)
-
-    plotFloatMap(
-        gdf,
-        dependentCol,
-        title="Uplink Throughput (Mbps) I45 Ookla Data",
-        zoom=11,
-        useLogScale=True,
-        alpha=0.1,
-    )
-
-    gdf[threshholdCol] = gdf[dependentCol] >= 15
-    customLabels = {False: "<15 mbps", True: ">=15 mbps"}
-    plotCategoricalMap(
-        gdf,
-        threshholdCol,
-        title="Uplink Throughput (Mbps) I45 Ookla Data",
-        zoom=11,
-        alpha=0.1,
-        customLabels=customLabels,
-    )
-
-
-def main():
-    stockholmSodertaljeVisualization()
-    # ooklaE4Visualization()
-    # ooklaI45Visualization()
-    plt.show()
+    for threshhold in threshholds:
+        gdf[threshholdCol] = gdf[dependentCol] >= threshhold
+        customLabels = {False: f"<{threshhold} mbps", True: f">={threshhold} mbps"}
+        plotCategoricalMap(
+            gdf,
+            threshholdCol,
+            title=f"Uplink Bitrate (Mbps) Sthlm - Södertalje Drive Test {date}",
+            alpha=0.1,
+            customLabels=customLabels,
+        )
 
 
 main()

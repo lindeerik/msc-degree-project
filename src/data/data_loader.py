740ccb2b6de97a4c1b82e18027b57b72d39379e5
Erik Linde <erik.linde@einride.tech>
2024-11-08 11:15:23 +0100

    chore(data-loader): add wrapper to pandas read_csv
    add wrapper as it is used in several places and adding wrapper improves replaceability

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -9,15 +9,19 @@ import pandas as pd
 
 
 def loadDataCsv(filePath, emptyData):
-
     fileNames = glob.glob(os.path.join(filePath, "*.csv"))
     dataFrames = []
 
     for fileName in fileNames:
-        df = pd.read_csv(fileName, index_col=None, header=0, na_values=emptyData)
+        df = loadCsv(fileName, emptyData)
         dataFrames.append(df)
 
     return pd.concat(dataFrames, axis=0, ignore_index=True)
+
+
+def loadCsv(filePath, emptyData):
+    df = pd.read_csv(filePath, index_col=None, header=0, na_values=emptyData)
+    return df
 
 
 def loadDataParquet(filePath):

aefcf9ec2a671e6f61f3e8dbd1440e4e19785fa9
Erik Linde <erik.linde@einride.tech>
2024-11-11 13:58:36 +0100

    feat(models): add dropout to neural network
    add dropout between hidden layers in neural network as a measure against overfitting

--- src/models/neuralnetwork/architecture.py
+++ src/models/neuralnetwork/architecture.py
@@ -1,5 +1,5 @@
 """
-Neural network arhcitecture for predicting throughput
+Neural network arhcitecture for predicting throughput. Has two hidden layers of size 64, ReLu activation and dropout included. 
 """
 
 import torch
@@ -7,16 +7,19 @@ from torch import nn
 
 
 class ThroughputPredictor(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_size, dropout_rate=0.3):
         super().__init__()
 
-        # Define layers (no normalization layer)
-        self.fc1 = nn.Linear(input_size, 64)  # First dense layer
-        self.fc2 = nn.Linear(64, 64)  # Second dense layer
-        self.fc3 = nn.Linear(64, 1)  # Final dense layer (single output)
+        self.fc1 = nn.Linear(input_size, 64)
+        self.dropout1 = nn.Dropout(p=dropout_rate)
+        self.fc2 = nn.Linear(64, 64)
+        self.dropout2 = nn.Dropout(p=dropout_rate)
+        self.fc3 = nn.Linear(64, 1)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))  # First dense layer + ReLU
-        x = torch.relu(self.fc2(x))  # Second dense layer + ReLU
-        x = self.fc3(x)  # Final dense layer (no activation)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = torch.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = self.fc3(x)
         return x.squeeze(-1)

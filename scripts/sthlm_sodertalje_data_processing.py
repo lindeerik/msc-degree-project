9cd8a5ff1b2923780a154fa807bd2c0a46149618
Erik Linde <erik.linde@einride.tech>
2024-11-12 10:33:56 +0100

    fix: clean data
    remove public data from article
    also remove outliers in self-recorded data

--- scripts/sthlm_sodertalje_data_processing.py
+++ scripts/sthlm_sodertalje_data_processing.py
@@ -27,7 +27,7 @@ def cleanData241004(standardizeCoordinates=True):
     # drop rows due to tunnels
     indicesToDropTunnels = list(range(269, 363)) + list(range(530, 711))
     indicesToDrop = indicesToDropTestStops + indicesToDropTunnels
-    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates)
+    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates, 200)
 
 
 def cleanData241028(standardizeCoordinates=True):
@@ -38,7 +38,7 @@ def cleanData241028(standardizeCoordinates=True):
     indicesToDrop = (
         list(range(0, 7)) + list(range(4631, 5090)) + list(range(8942, 8959))
     )
-    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates)
+    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates, 200)
 
 
 def cleanData241029(standardizeCoordinates=True):
@@ -52,10 +52,17 @@ def cleanData241029(standardizeCoordinates=True):
         + list(range(6105, 6445))
         + list(range(8849, 8881))
     )
-    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates)
+    cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates, 200)
 
 
-def cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates=True):
+def cleanData(
+    fileName,
+    dirRaw,
+    dirClean,
+    indicesToDrop,
+    standardizeCoordinates=True,
+    threshhold=None,
+):
     dfRaw = pd.read_csv(dirRaw + fileName, na_values=["", "-"])
     df = dfRaw.dropna(axis=1, how="all")
     dfClean = df.drop(df.index[indicesToDrop])
@@ -65,6 +72,8 @@ def cleanData(fileName, dirRaw, dirClean, indicesToDrop, standardizeCoordinates=
         dfClean = dfClean[
             (dfClean["Longitude"] < lonLimit) & (dfClean["Latitude"] < latLimit)
         ]
+    if threshhold:
+        dfClean = dfClean[dfClean["UL_bitrate"] < threshhold * 1024]
     dfClean.to_csv(dirClean + fileName)
 
 

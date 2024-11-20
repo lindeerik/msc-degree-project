4b02cbf3fab19b8edc2676efeafaf09919f9dc5a
Erik Linde <erik.linde@einride.tech>
2024-11-20 09:50:16 +0100

    fix(visualization): save figure files with names
    allow function to take list of names which the figure files will be saved as

--- src/visualization/save_figures.py
+++ src/visualization/save_figures.py
@@ -7,15 +7,16 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-def saveFiguresWithDateTime(path):
+def saveFiguresWithDateTime(path, fileNames=None):
     timestamp = datetime.now().strftime("%Y%m%dT%H%M%SZ")
     folder = os.path.join(path, timestamp)
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    for i, figNum in enumerate(plt.get_fignums(), start=1):
+    for i, figNum in enumerate(plt.get_fignums()):
         fig = plt.figure(figNum)
-        fileName = f"figure{i}.png"
+        fileName = fileNames[i] if fileNames else f"figure{i+1}.png"
         filePath = os.path.join(folder, fileName)
         fig.savefig(filePath)
         print(f"Saved {fileName} to {folder}")
+    plt.close("all")
